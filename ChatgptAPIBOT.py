263a6354dd8431c35f72e92ce2f3a11ed1f3a753
W0lfGalaxy <132662695+W0lfGalaxy@users.noreply.github.com>
2023-05-08 11:16:52 +0100

    Add files via upload

---
+++ ChatgptAPIBOT.py
@@ -0,0 +1,21 @@
+import openai
+
+# Adicione sua chave de API aqui
+openai.api_key = "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"
+
+def generate_text(prompt):
+    completions = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=2048,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].text
+    return message.strip()
+
+# Exemplo de uso
+prompt = input('Faça sua pergunta para o ChatGPT: \n')
+print(generate_text(prompt))
